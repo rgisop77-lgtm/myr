e73318adb00bf359c90b815efd5e34f807a22bd0
rgisop77-lgtm <rgisop77@gmail.com>
2025-10-27 00:47:01 +0530

    Update generate.py

--- plugins/generate.py
+++ plugins/generate.py
@@ -42,9 +42,9 @@ async def get_user_input(client, user_id, prompt_text, timeout=600):
         await client.send_message(user_id, "⏱️ **Request timed out. Please try again.**")
         return None
 
-@Client.on_message(filters.private & ~filters.forwarded & filters.text)
+@Client.on_message(filters.private & ~filters.forwarded & filters.text & ~filters.command)
 async def capture_user_input(client, message):
-    """Capture messages from users waiting for input"""
+    """Capture messages from users waiting for input - skip commands"""
     user_id = message.from_user.id
     
     # If this user is waiting for input, store the message and signal the event
@@ -71,80 +71,5 @@ async def main(bot: Client, message: Message):
     phone_number_msg = await get_user_input(
         bot, 
         user_id, 
-        "<b>Please send your phone number which includes country code</b>\n<b>Example:</b> <code>+13124562345, +9171828181889</code>\n\n<b>Enter /cancel to cancel the process</b>",
-        timeout=300
-    )
+        "<b>Please send your phone number which includes country code</b>\n<b>Example:</b> <code>+13124562345, +9171828181889
     
-    if phone_number_msg is None or phone_number_msg.text == '/cancel':
-        return await message.reply('<b>Process cancelled!</b>')
-    
-    phone_number = phone_number_msg.text
-    client = Client(":memory:", API_ID, API_HASH)
-    await client.connect()
-    await message.reply("Sending OTP...")
-    
-    try:
-        code = await client.send_code(phone_number)
-        
-        phone_code_msg = await get_user_input(
-            bot,
-            user_id,
-            "Please check for an OTP in official telegram account. If you got it, send OTP here after reading the below format.\n\nIf OTP is `12345`, **please send it as** `1 2 3 4 5`.\n\n**Enter /cancel to cancel the process**",
-            timeout=600
-        )
-        
-    except PhoneNumberInvalid:
-        await message.reply('`PHONE_NUMBER` **is invalid.**')
-        return
-    
-    if phone_code_msg is None or phone_code_msg.text == '/cancel':
-        return await message.reply('<b>Process cancelled!</b>')
-    
-    try:
-        phone_code = phone_code_msg.text.replace(" ", "")
-        await client.sign_in(phone_number, code.phone_code_hash, phone_code)
-    except PhoneCodeInvalid:
-        await message.reply('**OTP is invalid.**')
-        return
-    except PhoneCodeExpired:
-        await message.reply('**OTP is expired.**')
-        return
-    except SessionPasswordNeeded:
-        two_step_msg = await get_user_input(
-            bot,
-            user_id,
-            '**Your account has enabled two-step verification. Please provide the password.\n\nEnter /cancel to cancel the process**',
-            timeout=300
-        )
-        
-        if two_step_msg is None or two_step_msg.text == '/cancel':
-            return await message.reply('<b>Process cancelled!</b>')
-        
-        try:
-            password = two_step_msg.text
-            await client.check_password(password=password)
-        except PasswordHashInvalid:
-            await message.reply('**Invalid Password Provided**')
-            return
-    
-    string_session = await client.export_session_string()
-    await client.disconnect()
-    
-    if len(string_session) < SESSION_STRING_SIZE:
-        return await message.reply('<b>Invalid session string</b>')
-    
-    try:
-        user_data = await db.get_session(message.from_user.id)
-        if user_data is None:
-            uclient = Client(":memory:", session_string=string_session, api_id=API_ID, api_hash=API_HASH)
-            await uclient.connect()
-            await db.set_session(message.from_user.id, session=string_session)
-    except Exception as e:
-        return await message.reply_text(f"<b>ERROR IN LOGIN:</b> `{e}`")
-    
-    await bot.send_message(message.from_user.id, "<b>Account Login Successfully.\n\nIf You Get Any Error Related To AUTH KEY Then /logout first and /login again</b>")
-
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
